4ec247fad3413e717c69a0912bd0a9afbc190116
matiur2120 <matiur211993@gmail.com>
2024-02-27 01:38:01 +0600

    create user registration api

--- backend/src/model/user.ts
+++ backend/src/model/user.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 
 export type UserType = {
@@ -9,10 +10,16 @@ export type UserType = {
 };
 
 const userSchema = new mongoose.Schema({
-  emal: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+});
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
 });
 
 const User = mongoose.model<UserType>("User", userSchema);
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import User from "../model/user";
 
 const router = express.Router();
 
-router.get("/register", async (req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   try {
     const user = await User.findOne({
       email: req.body.email,
@@ -13,9 +14,20 @@ router.get("/register", async (req: Request, res: Response) => {
     }
     const newUser = new User(req.body);
     await newUser.save();
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY as string,
+      { expiresIn: "1d" }
+    );
+    res.cookie("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+    return res.sendStatus(200);
   } catch (error) {
     console.log(error);
-    res.status(500).send({ message: "Something went wrong!" });
+    res.status(500).send(error);
   }
 });
 
